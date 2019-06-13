18b1e48fe5e75bf8ea1ac80e9881608ea2791e0c
mathisti <mathisti@stud.ntnu.no>
2019-06-13 14:17:33 +0200

    Names, postal codes etc. Merge.

--- souper.py
+++ souper.py
@@ -1,6 +1,7 @@
 import os
 import re
 import socket
+import csv
 from http.client import IncompleteRead
 from ssl import CertificateError
 from urllib.error import HTTPError, URLError
@@ -10,7 +11,7 @@ import pycld2
 from bs4 import BeautifulSoup
 from geoip2.database import Reader
 
-from languagemodel import LanguageModel
+from languagemodel import LanguageModel, split_and_clean
 
 reader = Reader('res/GeoIP2-Country.mmdb')
 
@@ -42,11 +43,63 @@ def reg(txt):
     counties = re.search(r"akershus|aust.?agder|buskerud|finnmark|hedmark|hordaland|møre|romsdal|nordland|oppland|oslo|"
                          r"rogaland|sogn|fjordane|telemark|troms|trøndelag|vest.?agder|vestfold|østfold",
                          txt, re.IGNORECASE) is not None
-    return phone or name or counties
+
+    # Names
+    # Etternavn
+    source = open("res/etternavn.csv")
+    rdr = csv.reader(source, delimiter = ";")
+    etternavn = ""
+    for row in rdr:
+        etternavn += row[0][0].upper() + row[0][1:].lower() + " |"
+
+    etternavn = etternavn[5:-1]
+    #print(etternavn)
+
+
+    # Fornavn gutter
+    source = open("res/guttenavn_alle.csv")
+    rdr = csv.reader(source, delimiter=";")
+    guttenavn = ""
+    for row in rdr:
+        guttenavn += " " + row[0][0].upper() + row[0][1:].lower() + " |"
+
+    guttenavn = guttenavn[5:-1]
+    #print(guttenavn)
+
+    # Fornavn jenter
+    source = open("res/jentenavn_alle.csv")
+    rdr = csv.reader(source, delimiter=";")
+    jentenavn = ""
+    for row in rdr:
+        jentenavn += " " + row[0][0].upper() + row[0][1:].lower() + " |"
+
+    jentenavn = jentenavn[5:-1]
+    #print(jentenavn)
+
+
+    rex = f"(({guttenavn})|({jentenavn}))({etternavn})"
+    #print(rex)
+    person_names = re.search(rex, txt) is not None
+
+
+    # Postal code + city
+    source = open("res/Postnummerregister-ansi.txt", encoding="iso 8859-1")
+    rdr = csv.reader(source, delimiter="\t")
+    postnummer = ""
+
+    for row in rdr:
+        postnummer += row[0] + " " + row[1] + "|"
+
+    postnummer = postnummer[:-1]
+
+    location = re.search(postnummer, txt, re.IGNORECASE) is not None
+
+    #phone or name or counties or person_names or location
+    return phone or name or counties or person_names or location
 
 
 if __name__ == '__main__':
-    model = LanguageModel(["no", "other"], weights="LM6.h5")
+    # model = LanguageModel(["no", "other"], weights="LM3.h5")
     for file in os.listdir("res/oos_liste_03.01.19"):
         if not re.match("uri_(\W|no)", file):
             f = open(f"res/oos_liste_03.01.19/{file}")
@@ -56,12 +109,6 @@ if __name__ == '__main__':
                     print(url)
                     # if geo(url):
                     #     print("Norwegian")
-
-                    # html = urlopen(url, timeout=10)
-
-                    # lang = pycld2.detect(url, isPlainText=False, returnVectors=True, debugHTML=True)
-
-                    # print(lang)
 
                     txt = get_text(url)
 
@@ -73,30 +120,16 @@ if __name__ == '__main__':
                     except pycld2.error:
                         print(f"ERROR")
 
-                    # split = [s for s in split_and_clean(txt)]
+                    split = [s for s in split_and_clean(txt)]
+
+                    for s in split:
+                        if reg(s):
+                            print(url)
+                            print(s)
 
                     # for s in split:
-                    #     if reg(s):
-                    #         print(url)
+                    #     if geo(s):
                     #         print(s)
-
-                    # for s in split:
-                    #     # print(s)
-                    #     try:
-                    #         lang = pycld2.detect(s, isPlainText=True, returnVectors=True)[2][0][0]
-                    #         if lang == "NORWEGIAN":
-                    #             print(s)
-                    #     except pycld2.error:
-                    #         print(f"ERROR: {s}")
-
-                    # pre = model.predict(split, raw=True)
-                    # for p, s in zip(pre, split):
-                    #     if p[0] > 0.5:
-                    #         print(p[0], s)
-
-                    # if get_info(url)["country_code"] == "NO":
-                    #     print(url)
-
-                except (HTTPError, CertificateError, URLError, ConnectionResetError, IncompleteRead):
+                except (HTTPError, CertificateError, URLError, ConnectionResetError, IncompleteRead, socket.timeout):
                     pass
                     # print("Error")

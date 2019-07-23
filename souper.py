53b0ce072b99b1c496985dc6547ab89f6ab10a3a
mathisti <mathisti@stud.ntnu.no>
2019-07-23 13:16:53 +0200

    Added timer

--- souper.py
+++ souper.py
@@ -12,6 +12,7 @@ from time import sleep
 from urllib.error import HTTPError, URLError
 from urllib.parse import urlparse, urlunparse
 from urllib.request import urlopen, Request
+import time
 
 import pandas as pd
 import pycld2
@@ -137,7 +138,7 @@ def has_norwegian_version(connection):
         (parsed.scheme, new_base_url, parsed.path, parsed.params, parsed.query, parsed.fragment))
 
     try:
-        redir = requests.get(new_url)
+        redir = requests.get(new_url, timeout=10)
 
         if (redir.status_code == HTTPStatus.OK
                 or redir.status_code == HTTPStatus.MOVED_PERMANENTLY
@@ -156,15 +157,16 @@ def has_norwegian_version(connection):
             if i > 0:
                 return i, new_connection
     except (requests.ConnectionError, requests.exceptions.ConnectionError,
-            requests.exceptions.ChunkedEncodingError, requests.exceptions.TooManyRedirects, socket.timeout):
+            requests.exceptions.ChunkedEncodingError, requests.exceptions.TooManyRedirects, socket.timeout,
+            requests.exceptions.ReadTimeout):
         pass
     return 0, None
 
 
 WebPageValues = namedtuple("WebPageValues",
-                           ["o_url", "r_url", "geo", "no_ver", "dom", "bytes", "percentage", "score", "nor_score",
-                            "postal_u", "postal_t", "phone_u", "phone_t", "county_u", "county_t", "names_u",
-                            "names_t", "norway_u", "norway_t", "kroner_u", "kroner_t", "raw_html"])
+                           ["o_url", "r_url", "geo", "no_ver", "dom", "bytes", "percentage", "score", "no_score",
+                            "nor_score", "postal_u", "postal_t", "phone_u", "phone_t", "county_u", "county_t", "names_u"
+                            , "names_t", "norway_u", "norway_t", "kroner_u", "kroner_t", "raw_html"])
 
 
 def normalize(x, midpoint):
@@ -209,23 +211,27 @@ class WebPage:
     def get_ip(self):
         return get_ip(self.redirect_url)
 
-    def is_norwegian(self):
-        norwegian = self.values().nor_score * 1.6
-        postal = self.values().postal_u * 2
-        phone = self.values().phone_u * 0.3
-        county = self.values().county_t * 0.6
-        names = self.values().names_u * 0.2
-        norway = self.values().norway_t * 0.15
-        kroner = self.values().kroner_u * 0.05
+    @staticmethod
+    def is_norwegian(nor_score, postal_u, phone_u, county_t, names_u, norway_t, kroner_u, geo_loc, dom):
+        # if dom == "no":
+        #     print(".no domain")
+        #     return 1.0
+        # else:
+        norwegian = nor_score * 1.6
+        postal = postal_u * 2
+        phone = phone_u * 0.3
+        county = county_t * 0.7
+        names = names_u * 0.2
+        norway = norway_t * 0.15
+        kroner = kroner_u * 0.05
         geo_score = 0
-        if self.geo_loc and self.geo_loc == "NO":
+        if geo_loc and geo_loc == "NO":
             geo_score = 1.0
         score = norwegian + postal + phone + county + names + norway + kroner + geo_score
         print("Total score:", score)
         score = normalize(score, 1.4)
-        print("Normalized score:", score)
 
-        print("Norwegian score:", norwegian, " (", self.values().nor_score, ")")
+        print("Norwegian score:", norwegian, " (", nor_score, ")")
         print("Postal score:", postal)
         print("Phone score:", phone)
         print("County score:", county)
@@ -233,7 +239,6 @@ class WebPage:
         print("Norway score:", norway)
         print("Kroner score:", kroner)
         print("Geo ip:", geo_score)
-
         return score
 
     def values(self):
@@ -254,26 +259,46 @@ class WebPage:
         norway = has_norway(txt)
         kroner = has_kroner(txt)
 
+        # u = unique, t = total
+        postal_u = len(postal)
+        postal_t = sum(postal.values())
+        phone_u = len(phone)
+        phone_t = sum(phone.values())
+        county_u = len(county)
+        county_t = sum(county.values())
+        name_u = len(name)
+        name_t = sum(name.values())
+        norway_u = len(norway)
+        norway_t = sum(norway.values())
+        kroner_u = len(kroner)
+        kroner_t = sum(kroner.values())
+        geo_loc = self.geo_loc
+
         txt = re.sub(r"[\s,\"'’`©]+", " ", self.get_text())  # Remove csv-troublesome characters
 
-        return WebPageValues(self.orig_url, self.redirect_url, self.geo_loc, self.norwegian_version, dom, b, p, s,
-                             nor_score, len(postal), sum(postal.values()), len(phone), sum(phone.values()), len(county),
-                             sum(county.values()), len(name), sum(name.values()), len(norway), sum(norway.values()),
-                             len(kroner), sum(kroner.values()), txt)
+        # if dom == 'no':
+        #     no_score = 1
+        # else:
+        no_score = self.is_norwegian(nor_score, postal_u, phone_u, county_t, name_u, norway_t, kroner_u, geo_loc, dom)
+
+        return WebPageValues(self.orig_url, self.redirect_url, geo_loc, self.norwegian_version, dom, b, p, s,
+                             no_score, nor_score, postal_u, postal_t, phone_u, phone_t, county_u, county_t, name_u,
+                             name_t, norway_u, norway_t, kroner_u, kroner_t, txt)
 
 
 def iter_urls(url):
     try:
         # print(url)
-
         wp = WebPage.from_url(url)
 
-        for k, v in wp.values()._asdict().items():
+        val = wp.values()
+
+        for k, v in val._asdict().items():
             d[k].append(v)
 
-        score = wp.is_norwegian()
+        score = val.no_score
 
-        print("Probability Norvegica:", score)
+        print("Probability Norvegica / Normalized score:", score)
 
         if score > 0.70:
             print("It's på norsk", url, "\n")
@@ -284,52 +309,85 @@ def iter_urls(url):
         else:
             print("Not norsk:", url, "\n")
 
-    except (HTTPError, CertificateError, URLError, ConnectionResetError, IncompleteRead, socket.timeout) as e:
+    except (HTTPError, CertificateError, URLError, ConnectionResetError, IncompleteRead, socket.timeout,
+            socket.gaierror) as e:
         print(url, e)
         pass
 
 
 if __name__ == '__main__':
-    # df = pd.DataFrame.from_csv("uri_scores.csv", index_col=None)
-    #
-    # print(df.iloc[25545])
-    #
-    # df = df[df.r_url != df.iloc[25545].r_url]
-    #
-    # # print(df.iloc[25545])
-    # df = df.drop(["raw_html"], axis=1)
-    #
-    # df.to_csv("uri_scores_notxt.csv", index=False)
-    #
-    # exit(0)
-
-    d = {k: [] for k in WebPageValues._fields}
-    files = os.listdir("res/oos_liste_03.01.19")
+    df = pd.DataFrame.from_csv("uri_scores_full.csv", index_col=None)
 
     urls = []
 
-    for file in files:
-        if not re.match(r"uri_(\W|_)", file):
-            f = open(f"res/oos_liste_03.01.19/{file}")
+    d = {k: [] for k in WebPageValues._fields}
 
-            urls += [url.strip() for url in f]
+    for i, row in df.iterrows():
+        # print(row[["o_url", "r_url"]].values)
+        final_score = row["no_score"]
+        original_url = row["o_url"]
+        # if 0.6 > final_score > 0.4:
+        #     # print(url, final_score)
+        urls += original_url.split()
 
     random.shuffle(urls)
 
     print(len(urls))
-    for url in urls:
-        iter_urls(url)
+    num_iterations = 100
+    total_time = 0.0
+    for original_url in urls[:num_iterations]:
+        start = time.time()
+        iter_urls(original_url)
+        stop = time.time()
+        duration = stop - start
+        print("Time spent:", duration, "\n")
+        total_time += duration
 
-    with ThreadPoolExecutor(max_workers=16) as pool:
-        pool.map(iter_urls, urls, timeout=120)
-        print("Sleeping")
-        sleep(65000)
-        print("Slept")
-        df = pd.DataFrame.from_dict(d)
-        df.to_csv("uri_scores.csv", index=False)
-        print("CSV saved")
-        pool.shutdown(False)
+    # start = time.time()
+    # iter_urls("http://www.hiab.at")
+    # stop = time.time()
+    # duration = stop - start
+    # print("Time spent:", duration, "\n")
+    print("Total time:", total_time)
+    print("Average time:", total_time/num_iterations)
 
-    # in case it actually finishes
     df = pd.DataFrame.from_dict(d)
     df.to_csv("uri_scores.csv", index=False)
+    exit(0)
+    # iter_urls("http://www.eflorist.co.uk")
+
+    # d = {k: [] for k in WebPageValues._fields}
+    # files = os.listdir("res/oos_liste_03.01.19")
+
+    # urls = []
+
+    # for file in files:
+    #     if not re.match(r"uri_(\W|_)", file):
+    #         f = open(f"res/oos_liste_03.01.19/{file}")
+    #
+    #         urls += [url.strip() for url in f]
+    #
+    # random.shuffle(urls)
+
+    # print(len(urls))
+    # for url in urls[:100]:
+    #     iter_urls(url)
+
+    # start = time.time()
+    # with ThreadPoolExecutor(max_workers=16) as pool:
+    #     pool.map(iter_urls, urls, timeout=120)
+    #     print("Sleeping")
+    #     sleep(54000)
+    #     print("Slept")
+    #     df = pd.DataFrame.from_dict(d)
+    #     df.to_csv("uri_scores.csv", index=False)
+    #     print("CSV saved")
+    #     pool.shutdown(False)
+
+    # stop = time.time()
+    # duration = stop - start
+    # print("Time spent:", duration, "\n")
+    #
+    # # in case it actually finishes
+    # df = pd.DataFrame.from_dict(d)
+    # df.to_csv("uri_scores.csv", index=False)
